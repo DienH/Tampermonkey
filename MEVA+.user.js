9e2c2ffa938fd44cbba88da0294586ce1b62d4f4
DienH <31889811+DienH@users.noreply.github.com>
2022-06-29 15:47:07 +0200

    Update MEVA+.user.js

--- MEVA+.user.js
+++ MEVA+.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         MEVA+
 // @namespace    http://tampermonkey.net/
-// @version      0.2.85
+// @version      0.2.86
 // @description  Help with MEVA
 // @author       Me
 // @match        http*://meva/*
