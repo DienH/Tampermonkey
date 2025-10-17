2fa6f3b1ccef6a06a5daba05087efab8e5bd7669
DienH <31889811+DienH@users.noreply.github.com>
2025-10-17 11:24:38 +0200

    Update Easily+.user.js

--- Easily+.user.js
+++ Easily+.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Easily+
 // @namespace    http://tampermonkey.net/
-// @version      1.0.251014
+// @version      1.0.251015
 // @description  Easily plus facile
 // @author       You
 // @match        https://easily-prod.chu-clermontferrand.fr/*
